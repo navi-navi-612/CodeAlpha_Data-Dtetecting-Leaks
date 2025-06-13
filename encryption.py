975fc5aedcf7a56a1db30d5b9b0c0930e0123b2c
navi-navi-612 <137594495+navi-navi-612@users.noreply.github.com>
2025-06-13 14:08:57 +0530

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,13 @@
+from Crypto.Cipher import AES
+import base64
+import os
+
+key = os.environ.get("AES_SECRET_KEY", "12345678901234567890123456789012").encode('utf-8')  # 32 bytes
+
+def pad(data):
+    return data + (16 - len(data) % 16) * chr(16 - len(data) % 16)
+
+def encrypt_data(data):
+    cipher = AES.new(key, AES.MODE_ECB)
+    encrypted = cipher.encrypt(pad(data).encode('utf-8'))
+    return base64.b64encode(encrypted).decode('utf-8')
